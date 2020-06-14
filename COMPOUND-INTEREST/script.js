a08b30908a4440fbefbd35581a5b61c26185f124
ms606 <muffi606@gmail.com>
2020-06-14 23:24:18 +0500

    Add files via upload

---
+++ COMPOUND-INTEREST/script.js
@@ -0,0 +1,16 @@
+
+
+
+function generateTable(form){
+    var amount;
+    var rate;
+    var years;
+    var interest; 
+    var table;
+    var year = 1;
+
+    amount = document.getElementById('deposit').value;
+
+    console.log(amount);
+}
+
