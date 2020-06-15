21f1ff6b991f25d82b4a0914102eaeffd9c9396b
ms606 <muffi606@gmail.com>
2020-06-15 17:18:33 +0500

    Add files via upload

--- COMPOUND-INTEREST/script.js
+++ COMPOUND-INTEREST/script.js
@@ -10,7 +10,21 @@ function generateTable(form){
     var year = 1;
 
     amount = document.getElementById('deposit').value;
-
+    rate   = document.getElementById('rate').value;
+    years  = document.getElementById('years').value;
+    
     console.log(amount);
+    console.log(rate);
+    console.log(years);
+
+    table = 
+    	`<table>
+    		<tr>
+    			<th>Year</th>
+    		</tr>
+    	 </table>	
+    	`
+
+	document.getElementById("result").innerHTML = table;
 }
 
