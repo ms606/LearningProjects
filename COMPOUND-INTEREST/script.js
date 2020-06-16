ef499808779594f070447a607de46ef3ec83e822
ms606 <muffi606@gmail.com>
2020-06-16 13:34:53 +0500

    Add files via upload

--- COMPOUND-INTEREST/script.js
+++ COMPOUND-INTEREST/script.js
@@ -1,30 +1,42 @@
-
-
-
 function generateTable(form){
-    var amount;
-    var rate;
-    var years;
-    var interest; 
+    let amount;
+    let rate;
+    let years;
     var table;
     var year = 1;
+
+    let interest;
 
     amount = document.getElementById('deposit').value;
     rate   = document.getElementById('rate').value;
     years  = document.getElementById('years').value;
     
-    console.log(amount);
-    console.log(rate);
-    console.log(years);
+    // console.log(amount);
+    // console.log(rate);
+    // console.log(years);
 
     table = 
     	`<table>
     		<tr>
-    			<th>Year</th>
+                <th>Year</th> <th>Starting Value</th> 
+                <th>Interest Earned</th> <th>Ending Value</th>
     		</tr>
-    	 </table>	
-    	`
+    	
+        `
+
+    while (year <= years) {
+        table += '<tr>';
+        table += '<td>' + years + '</td>';
+        table += `<td> $${amount}</td>`;
+        interest = (rate/100)*amount;
+        interest = +interest;
+        table += `<td>${interest}</td>`;    
+        table += `<td>${interest+amount}</td>`;
+        table += '</tr>';
+        year++
+    }    
+
+    table += '</table>';
 
 	document.getElementById("result").innerHTML = table;
 }
-
